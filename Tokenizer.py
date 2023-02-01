cacb16e4c544abcfd29dc2b55d5aa76fdf036a2d
Haseeb <haseeb_heaven@yahoo.com>
2023-02-01 08:20:33 +0400

    Updated Tokenizer with UI changes
    Optimized the code little bit and fixed the UI

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,14 +1,11 @@
 """ Tokenizer is a simple GUI that allows you to tokenize text using the GPT-2 Tokenizer.
-    It will output the tokens, token type IDs, attention mask, length and number of characters.
+    It will output the tokens, token type IDs, attention mask.
     It also calculates the price per token based on the model you choose.
     Written by: @Haseeb Mir.
-    Tools used: Python 3.9.7, PyCharm 2021.2.3, Tkinter, Transformers 4.11.3, Wonderwords 1.0.1.
-    VSC Version: 1.60.2.
     Dated 2021-09-13.
 """
 
 # Import modules
-import string
 from tkinter import messagebox
 from tkinter import scrolledtext
 import tkinter as tk
@@ -19,87 +16,60 @@ from wonderwords import RandomSentence
 root = tk.Tk()
 root.title("Tokenizer")
 root.geometry("350x580")
+
 # Disable maximize button
 root.resizable(0, 0)
 fonts_arial = "Arial 14 bold"
 
-# Create dictonary of prices for each token Davinci = 0.0200 / 1000 tokens, Curie = 0.0020/ 1000 tokens, Babbage = 0.0005 / 1000 tokens, Ada = 0.0004 / 1000 tokens
+# Dictonary of prices for each token Davinci = 0.0200 / 1000 tokens, Curie = 0.0020/ 1000 tokens, Babbage = 0.0005 / 1000 tokens, Ada = 0.0004 / 1000 tokens
 prices = {"davinci": (0.0200/ 1000), "curie": (0.0020/1000), "babbage": (0.0005/1000), "ada":(0.0004/1000)}
+
+# This is helper method that updates the text box UI.
+def update_text_box(text_box, font, value):
+    text_box.configure(state="normal")
+    text_box.delete(1.0, tk.END)
+    text_box.configure(font=font)
+    text_box.insert(tk.INSERT, value)
+    text_box.configure(state="disabled")
 
 # This method tokenizes the given text using the GPT-2 Tokenizer. It then outputs the tokens, token type IDs, attention mask, length and number of characters.
 def tokenize_input():
-    
-    '''
-    This function tokenizes the given text using the GPT-2 Tokenizer. 
-    It then outputs the tokens, token type IDs, attention mask, length and number of characters.
-    '''
-    
-    tokenizer = GPT2TokenizerFast.from_pretrained(model_entry.get())
+    input_model = model_entry.get()
+    tokenizer = GPT2TokenizerFast.from_pretrained(input_model)
     input_text = input_textbox.get("1.0", tk.END)
     token_dict = tokenizer(input_text)
     tokens = token_dict['input_ids']
     attention_mask = token_dict['attention_mask']
     token_length = len(tokens)
     characters = len(input_text)
-    input_ids_output.configure(state="normal")
-    input_ids_output.delete(1.0, tk.END)
-    input_ids_output.configure(font=fonts_arial)
-    input_ids_output.insert(tk.INSERT, f"{tokens}")
-    input_ids_output.configure(state="disabled")
 
-    attention_mask_output.configure(state="normal")
-    attention_mask_output.delete(1.0, tk.END)
-    attention_mask_output.configure(font=fonts_arial)
-    attention_mask_output.insert(tk.INSERT, f"{attention_mask}")
-    attention_mask_output.configure(state="disabled")
+    update_text_box(input_ids_output, fonts_arial, f"{tokens}")
+    update_text_box(attention_mask_output, fonts_arial, f"{attention_mask}")
+    update_text_box(token_output, fonts_arial, f"{token_length}")
+    update_text_box(characters_output, fonts_arial, f"{characters}")
 
-    token_output.configure(state="normal")
-    token_output.delete(1.0, tk.END)
-    token_output.configure(font=fonts_arial)
-    token_output.insert(tk.INSERT, f"{token_length}")
-    token_output.configure(state="disabled")
-
-    characters_output.configure(state="normal")
-    characters_output.delete(1.0, tk.END)
-    characters_output.configure(font=fonts_arial)
-    characters_output.insert(tk.INSERT, f"{characters}")
-    characters_output.configure(state="disabled")
-    
-    price_output.configure(state="normal")
-    price_output.delete(1.0, tk.END)
-    price_output.configure(font=fonts_arial)
     price_per_token = token_length * prices["davinci"]
-    price_output.insert(tk.INSERT, f"{price_per_token:.8f} USD")
-    price_output.configure(state="disabled")
+    update_text_box(price_output, fonts_arial, f"{price_per_token:.5f}")
 
 def show_help():
-    '''
-    This function will show a help messagebox 
-    '''
-    messagebox.showinfo("Help", "This application tokenizes the given text using the GPT-2 Tokenizer. It will output the tokens, token type IDs, attention mask, length and number of characters.")
+    messagebox.showinfo("Help", "This application tokenizes the given text using the GPT-2 Tokenizer. It will output the tokens, token type IDs, attention mask information.")
 
 # This method generates a random sentence using the Wonderwords module.
 def generate_example():
-    '''
-    This fuction will generate a random string of characters
-    '''
     random_sentence = RandomSentence()
-    example_text = random_sentence.sentence()
+    example_text:str = ""
+    
+    for i in range(5):
+        example_text += random_sentence.sentence() + "\n"
     input_textbox.delete("1.0", tk.END)
     input_textbox.insert(tk.INSERT, example_text)
 
 # These are helper functions to add and delete data from the entry boxes
 def add_data_entry(entry:tk.Entry, data):
-    '''
-    This function will add the given data to the entry
-    '''
     delete_data_entry(entry)
     entry.insert(0, data)
 
 def delete_data_entry(entry:tk.Entry):
-    '''
-    This function will delete the data from the given entry
-    '''
     entry.delete(0, tk.END)
 
 # Setting up the GUI.
@@ -124,31 +94,31 @@ help_button.pack()
 example_button = tk.Button(root, text="Show Examples", command=generate_example)
 example_button.pack()
 
-input_ids_label = tk.Label(root, text="Input IDs:")
+input_ids_label = tk.Label(root, text="INPUT ID")
 input_ids_label.pack()
 
 input_ids_output = scrolledtext.ScrolledText(root, width=50, height=2, state="disabled")
 input_ids_output.pack()
 
-attention_mask_label = tk.Label(root, text="Attention Mask:")
+attention_mask_label = tk.Label(root, text="ATTENTION MASK:")
 attention_mask_label.pack()
 
 attention_mask_output = scrolledtext.ScrolledText(root, width=50, height=2, state="disabled")
 attention_mask_output.pack()
 
-token_label = tk.Label(root, text="Tokens:")
+token_label = tk.Label(root, text="TOKENS")
 token_label.pack()
 
 token_output = scrolledtext.ScrolledText(root, width=50, height=2, state="disabled")
 token_output.pack()
 
-characters_label = tk.Label(root, text="Characters:")
+characters_label = tk.Label(root, text="CHARACTERS")
 characters_label.pack()
 
 characters_output = scrolledtext.ScrolledText(root, width=50, height=2, state="disabled")
 characters_output.pack()
 
-price_label = tk.Label(root, text="Price in $:")
+price_label = tk.Label(root, text="PRICE")
 price_label.pack()
 
 price_output = scrolledtext.ScrolledText(root, width=50, height=2, state="disabled")
